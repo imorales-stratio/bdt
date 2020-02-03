567900444d04d75fc5464d43ed228b2dc7502b7f
Iván Morales <imorales@stratio.com>
2020-02-03 11:38:05 +0100

    Fix error in Rocket SSO login

--- src/main/java/com/stratio/qa/specs/DcosSpec.java
+++ src/main/java/com/stratio/qa/specs/DcosSpec.java
@@ -133,7 +133,9 @@ public class DcosSpec extends BaseGSpec {
                 tokenList = new String[]{"user", "dcos-acs-auth-cookie", "stratio-governance-auth"};
             }
             List<com.ning.http.client.cookie.Cookie> cookiesAtributes = addSsoToken(ssoCookies, tokenList);
-            ThreadProperty.set("dcosAuthCookie", ssoCookies.get("dcos-acs-auth-cookie"));
+            if (ssoCookies.get("dcos-acs-auth-cookie") != null) {
+                ThreadProperty.set("dcosAuthCookie", ssoCookies.get("dcos-acs-auth-cookie"));
+            }
             commonspec.setCookies(cookiesAtributes);
         }
     }
